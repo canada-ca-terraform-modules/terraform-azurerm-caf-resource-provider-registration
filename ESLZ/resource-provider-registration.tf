f451b3d2426fe0ef0b8e16fe814e98f6020bde48
Bernard Maltais <bernard.maltais@ssc-spc.gc.ca>
2025-04-30 11:15:09 -0400

    Fix typos

--- ESLZ/resource-provider-registration.tf
+++ ESLZ/resource-provider-registration.tf
@@ -1,5 +1,5 @@
 variable "resource_provider_registration" {
-  description = "Details about esource_provider_registration config"
+  description = "Details about resource_provider_registration config"
   type        = any
   default     = {}
 }
@@ -9,5 +9,5 @@ module "resource_provider_registration" {
   source   = "github.com/canada-ca-terraform-modules/terraform-azurerm-caf-resource-provider-registration?ref=v1.0.0"
 
   name     = each.key
-  features = try(each.value.features, [])
+  features = try(each.value.features, {})
 }
